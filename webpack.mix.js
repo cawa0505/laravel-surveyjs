2656752f00c8ed6d4a315b957eff348a8b576343
Jimmy Yen <zeng.tw@gmail.com>
2019-10-25 10:49:04 +0800

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,3 +11,6 @@ mix
     .setPublicPath('public')
     .js('resources/assets/js/survey-manager.js', 'public/js/')
     .js('resources/assets/js/survey-front.js', 'public/js/');
+
+mix
+    .copy('node_modules/survey-vue/survey.css', 'public/css/');
